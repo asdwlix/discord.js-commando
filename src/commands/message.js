7fbd4e286c419e5e27841810ff2ecc767409b8af
asdwlix <40040813+asdwlix@users.noreply.github.com>
2018-06-07 21:40:39 +0200

    Update message.js

--- src/commands/message.js
+++ src/commands/message.js
@@ -216,7 +216,8 @@ class CommandMessage {
 				const invite = this.client.options.invite;
 				return this.reply(stripIndents`
 					Komutu kullanırken bir hata oluştu!: \`${err.name}: ${err.message}\`
-					Lütfen bildirin: https://discord.gg/GvfuXmE
+					Lütfen bildirin: https://discord.gg/EGxHua6
+					Sahib: AlperS#4640
 				`);
 			}
 		}
